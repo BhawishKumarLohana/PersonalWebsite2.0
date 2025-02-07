4dcf2698e774eac92f6e228a4f46036d876f58e5
Bhawish Kumar <bhawishkumar000@gmail.com>
2025-02-08 01:22:33 +0530

    fav

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head><link rel="icon" href="/logo.png" /></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
